3ab332a47ce7d801433fbfabfbe8024cf465e7a0
ammarzeyad <ammarramadan16@yahoo.com>
2021-09-14 19:29:19 +0300

    lab03

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import newArra from './Data.json';
-
+import {Modal,Button} from 'react-bootstrap'
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            beastName: { title: '', image_url: '', description: '' },
+            showModal: false
+        }
+    }
 
+    handleClose = () => {
+        this.setState({
+            showModal: false
+        })
+    }
+
+    beastNameButton = (title, image_url, description) => {
+        this.setState({
+            beastName: { title, image_url, description },
+            showModal: true
+        })
+    }
     render() {
 
-        return(
+        return (
             <div>
-                {newArra.map((element,index) =>{
-                    return( <HornedBeast key={index} title={element.title}
-                        image_url={element.image_url} description ={element.description}
-                        keyword ={element.keyword} horns ={element.horns} />)
-                    
+                {newArra.map((element, index) => {
+                    return (<HornedBeast key={index} title={element.title}
+                        image_url={element.image_url} description={element.description}
+                        keyword={element.keyword} horns={element.horns}
+                        beastNameButton={this.beastNameButton} />)
+
                 })}
+                <Modal show={this.state.showModal} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.state.beastName.title}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <img src={this.state.beastName.image_url} alt={this.state.beastName.title} style={{width:'100%'}}/>
+                        <br />
+                        {this.state.beastName.description}
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
 
