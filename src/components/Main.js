9331d31daab51f2e1d2e2b5eea5204e387e61305
ammarzeyad <ammarramadan16@yahoo.com>
2021-10-03 00:50:35 +0300

    done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,61 +1,112 @@
-import React from 'react';
-import HornedBeast from './HornedBeast';
-import newArra from './Data.json';
-import {Modal,Button} from 'react-bootstrap'
-import FormModels from './FormModels';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HornedBeast from "./HornedBeast";
+import arrayData from "./Data.json"
+import { Modal, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap/";
+
 class Main extends React.Component {
+
+
     constructor(props) {
         super(props);
         this.state = {
-            beastName: { title: '', image_url: '', description: '' },
-            showModal: false
+            showModal: false,
+                i: 0,
+                fil: [],
+            obj: {
+                title: '',
+                img: '',
+                Parg: ''
+            }
         }
     }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.setState({
+            i : Number(event.target.value),
+            fil: arrayData.filter(element => element.horns === Number(event.target.value)),
+        });
+    };
+
     handleClose = () => {
         this.setState({
             showModal: false
         })
     }
 
-    beastNameButton = (title, image_url, description) => {
+    beastNameButton = (title, img, Parg) => {
         this.setState({
-            beastName: { title, image_url, description },
-            showModal: true
+            showModal: true,
+            obj: { title, img, Parg }
         })
     }
+
     render() {
-
         return (
-            <div>
-                <FormModels /> 
-                {newArra.map((element, index) => {
-                    return (<HornedBeast key={index} title={element.title}
-                        image_url={element.image_url} description={element.description}
-                        keyword={element.keyword} horns={element.horns}
-                        beastNameButton={this.beastNameButton} />)
+            <div style={{ backgroundColor: "lightgray" }}>
 
-                })}
+                <Form onChange={this.submitHandler}>
+                    <Form.Label>select the horns</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        <option value="0">All</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="100">100</option>
+                    </Form.Select>
+                </Form>
 
-                
+
+                {this.state.i === 0 ? arrayData.map((element, ind) => {
+                    return (
+                        <HornedBeast
+                            key={ind}
+                            title={element.title}
+                            img={element.image_url}
+                            Parg={element.description}
+                            keyword={element.keyword}
+                            horns={element.horns}
+                            beastNameButton={this.beastNameButton} />
+                    )
+                }) : this.state.fil.map((element, ind) => {
+                    return (
+                        <HornedBeast
+                            key={ind}
+                            title={element.title}
+                            img={element.image_url}
+                            Parg={element.description}
+                            keyword={element.keyword}
+                            horns={element.horns}
+                            beastNameButton={this.beastNameButton} />
+                    )
+                })
+                }
+
+
                 <Modal show={this.state.showModal} onHide={this.handleClose}>
                     <Modal.Header closeButton>
-                        <Modal.Title>{this.state.beastName.title}</Modal.Title>
+                        <Modal.Title>{this.state.obj.title}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <img src={this.state.beastName.image_url} alt={this.state.beastName.title} style={{width:'100%'}}/>
+                        <img className="img" src={this.state.obj.img} alt={this.state.obj.title} style={{ width: '100%' }} />
                         <br />
-                        {this.state.beastName.description}
+                        <br />
+                        {this.state.obj.Parg}
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={this.handleClose}>
-                            Close
-                        </Button>
+                        <Button onClick={this.handleClose} variant="secondary">Close</Button>
                     </Modal.Footer>
                 </Modal>
             </div>
-        )
 
+
+        )
     }
+
+
 }
+
 
 export default Main;
